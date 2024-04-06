60e05b4ebbd5cab36f0d3f604e673950b208e13a
JamieVaughn <wxv6@case.edu>
2024-04-05 21:44:26 -0400

    try base path '.'

--- src/config.mjs
+++ src/config.mjs
@@ -4,7 +4,7 @@ const CONFIG = {
   name: 'Make 🏗️ Devs',
 
   origin: 'https://wjv.io',
-  basePathname: '/blog', // '/blog'
+  basePathname: '/', // '/blog'
   trailingSlash: false,
 
   title: 'Astro Blog',
